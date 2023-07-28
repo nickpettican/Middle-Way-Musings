f90a417053297d336247042c037fe8f88faaf292
Nicolas Pettican <nicolaspettican@gmail.com>
2023-07-28 14:15:10 +0530

    Fix echo new line issue

--- deploy.sh
+++ deploy.sh
@@ -18,20 +18,20 @@ while [ $# -gt 0 ]; do
 done
 
 if [ -z "$message" ]; then
-    echo "Please provide a message as -m 'Example message'\n"
+    echo -e "Please provide a message as -m 'Example message'\n"
     exit 1
 fi
 
-echo "Updating submodule/n"
+echo -e "Updating submodule/n"
 
 git submodule update --init --recursive
 
-echo "Building site"
+echo -e "Building site"
 
 hugo -t paper --minify
 
-echo "Pushing to production repo\n"
-echo "Using message '$message'\n"
+echo -e "Pushing to production repo\n"
+echo -e "Using message '$message'\n"
 
 cd public/
 git add .
@@ -39,10 +39,10 @@ git commit -m "$message"
 git push origin main
 cd ..
 
-echo "Successfully deployed to production repo.\n"
-echo "Updating parent repo\n"
+echo -e "Successfully deployed to production repo.\n"
+echo -e "Updating parent repo\n"
 
 git add .
 git commit -m "Deploy with message '$message'"
 git push origin main
-echo "Successfully updated parent repo.\n"
+echo -e "Successfully updated parent repo.\n"
